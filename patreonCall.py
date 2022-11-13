d5ffe225f5ccb302027d2a4ccb4ad4564f13d8fd
Darryl Davis <ddavis1306@users.noreply.github.com>
2022-11-13 14:20:58 -0800

    prints patrons in list by order of donations

--- patreonCall.py
+++ patreonCall.py
@@ -5,22 +5,51 @@ import requests
 from config import *
 import patreon
 from flask import request
-
+#auth patreon api
 api_client = patreon.API(access_token)
 
 campaign_response = api_client.fetch_campaign()
 campaign_id = campaign_response.data()[0].id()
 
 all_pledges = []
+#cursor None forces api to look through multiple pages of patrons
 cursor = None
+#filters pledges that have active ccd and the amout they pledged
 while True:
-    pledges_response = api_client.fetch_page_of_pledges(campaign_id, 25, cursor=cursor)
+    pledges_response = api_client.fetch_page_of_pledges(campaign_id,
+     25, 
+     cursor=cursor,
+     fields= {'pledge': ['total_historical_amount_cents' , 'declined_since']}
+     )
+
     cursor = api_client.extract_cursor(pledges_response)
     all_pledges += pledges_response.data()
     if not cursor:
         break
 
+pledges_info = []
 
 for pledge in all_pledges:
-    print(pledge.relationship('patron').attribute('first_name'))
+    declined = pledge.attribute('declined_since')
+    reward_tier = 0
+#sorts the reward teir of patrons
+    if pledge.relationships()['reward']['data']:
+        reward_tier = pledge.relationship('reward').attribute('amount_cents')
 
+    if not declined and reward_tier >= 100:
+        pledges_info.append({
+            'first_name': pledge.relationship('patron').attribute('first_name'),
+            'total_historical_amount_cents': pledge.attribute('total_historical_amount_cents')
+        }) 
+
+sorted_pledges = sorted(
+    pledges_info,
+    key=lambda pledge: pledge['total_historical_amount_cents'],
+    reverse=True
+)
+
+pledge_names = [pledge['first_name'] for pledge in sorted_pledges]
+print (pledge_names)
+#prints names of patrons
+# for pledge in pledges_info:
+#     print(pledge['first_name'])
